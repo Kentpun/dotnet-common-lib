d243c077ab4906673bbf06b3d6040b84fe38e7c3
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-04-04 14:07:14 +0800

    update

--- HKSH.NugetRepository/HKSH.Common/ShareModel/MessageResult.cs
+++ HKSH.NugetRepository/HKSH.Common/ShareModel/MessageResult.cs
@@ -4,27 +4,45 @@
     /// MessageResult
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    /// <seealso cref="MessageResult" />
     public class MessageResult<T> : MessageResult
     {
         /// <summary>
-        /// the return data
+        /// Gets or sets the data.
         /// </summary>
+        /// <value>
+        /// The data.
+        /// </value>
         public new T? Data { get; set; }
 
         /// <summary>
         /// Successes the result.
         /// </summary>
         /// <param name="data">The data.</param>
+        /// <param name="messageModel">The message model.</param>
         /// <returns></returns>
-        public static MessageResult<T> SuccessResult(T? data = default)
+        public static MessageResult<T> SuccessResult(T? data = default, string? message = "")
         {
             return new MessageResult<T>
             {
                 Success = true,
                 Data = data,
-                Message = "操作成功！",
-                Code = 1
+                Message = string.IsNullOrEmpty(message) ? "Operation successful" : message
+            };
+        }
+
+        /// <summary>
+        /// return failure result
+        /// </summary>
+        /// <param name="messageModel">The message model.</param>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
+        public static MessageResult<T> FailureResult(T? data = default, string? message = "")
+        {
+            return new MessageResult<T>
+            {
+                Success = false,
+                Data = data,
+                Message = string.IsNullOrEmpty(message) ? "Operation failed" : message
             };
         }
     }
@@ -32,60 +50,60 @@
     /// <summary>
     /// MessageResult
     /// </summary>
+    /// <seealso cref="MessageResult" />
     public class MessageResult
     {
-        #region Fields
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="MessageResult"/> is success.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if success; otherwise, <c>false</c>.
+        /// </value>
+        public bool Success { get; set; } = true;
 
         /// <summary>
-        /// success flag
+        /// Gets or sets the message.
         /// </summary>
-        public bool? Success { get; set; }
-
-        /// <summary>
-        /// Gets or sets the code.
-        /// </summary>
-        public int? Code { get; set; }
-
-        /// <summary>
-        /// message code model
-        /// </summary>
+        /// <value>
+        /// The message.
+        /// </value>
         public string? Message { get; set; }
 
         /// <summary>
-        /// the return data
+        /// Gets or sets the data.
         /// </summary>
+        /// <value>
+        /// The data.
+        /// </value>
         public object? Data { get; set; }
-
-        #endregion Fields
 
         /// <summary>
         /// Successes the result.
         /// </summary>
         /// <param name="data">The data.</param>
+        /// <param name="message">The message.</param>
         /// <returns></returns>
-        public static MessageResult SuccessResult(object? data = default)
+        public static MessageResult SuccessResult(object? data = default, string? message = "")
         {
             return new MessageResult
             {
                 Success = true,
                 Data = data,
-                Message = "Operate successfully！",
-                Code = 1
+                Message = string.IsNullOrEmpty(message) ? "Operation successful" : message
             };
         }
 
         /// <summary>
-        /// return failure result
+        /// Failures the result.
         /// </summary>
+        /// <param name="message">The message.</param>
         /// <returns></returns>
-        public static MessageResult FailureResult(object? data = default)
+        public static MessageResult FailureResult(string? message = "")
         {
             return new MessageResult
             {
                 Success = false,
-                Message = "Internal server error, please contact the administrator！",
-                Code = 0,
-                Data = data
+                Message = string.IsNullOrEmpty(message) ? "Operation failed" : message
             };
         }
     }
