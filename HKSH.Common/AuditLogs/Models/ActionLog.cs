46333fc28d95702566d5becf43cd46bff7b24e6c
adu <adu@deloitte.com.cn>
2023-05-16 11:14:36 +0800

    添加操作日志实体

---
+++ HKSH.Common/AuditLogs/Models/ActionLog.cs
@@ -0,0 +1,82 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace HKSH.Common.AuditLogs.Models
+{
+    public class ActionLog
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// 类似于组装后的FormId(ALC.434.001).
+        /// </summary>
+        /// <value>
+        /// The section.
+        /// </value>
+        [BsonElement("ActionDate")]
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime ActionDate { get; set; } 
+
+        /// <summary>
+        /// 修改字段.
+        /// </summary>
+        /// <value>
+        /// The field.
+        /// </value>
+        [BsonElement("Field")]
+        public string Field { get; set; } = null!;
+
+        /// <summary>
+        /// 原始值.
+        /// </summary>
+        /// <value>
+        /// The update from.
+        /// </value>
+        [BsonElement("UpdateFrom")]
+        public string UpdateFrom { get; set; } = null!;
+
+        /// <summary>
+        /// 修改值.
+        /// </summary>
+        /// <value>
+        /// The update to.
+        /// </value>
+        [BsonElement("UpdateTo")]
+        public string UpdateTo { get; set; } = null!;
+
+        /// <summary>
+        /// 修改的时间.
+        /// </summary>
+        /// <value>
+        /// The update time.
+        /// </value>
+        [BsonElement("UpdateTime")]
+        public DateTime UpdateTime { get; set; }
+
+        /// <summary>
+        /// 修改的操作人.
+        /// </summary>
+        /// <value>
+        /// The update by.
+        /// </value>
+        [BsonElement("UpdateBy")]
+        public string UpdateBy { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the operator.
+        /// </summary>
+        /// <value>
+        /// The operator.
+        /// </value>
+        [BsonElement("Action")]
+        public string Action { get; set; } = null!;
+    }
+}
