8e34e98f531b07db9891894834b0bb597a257bc0
adu <adu@deloitte.com.cn>
2023-05-30 18:16:53 +0800

    增加通用字段和cap配置

---
+++ HKSH.Common/CommonEnum/TaskType.cs
@@ -0,0 +1,54 @@
+﻿//  Mou,Xiaohua 2023/04/03 16:56
+
+using System.ComponentModel;
+
+namespace HKSH.Common.CommonEnum
+{
+    /// <summary>
+    /// TaskType
+    /// </summary>
+    public enum TaskType
+    {
+        /// <summary>
+        /// The device
+        /// </summary>
+        [Description("Device")] Device = 1001,
+
+        /// <summary>
+        /// The location
+        /// </summary>
+        [Description("Location")] Location = 1002,
+
+        /// <summary>
+        /// The role
+        /// </summary>
+        [Description("Role")] Role = 1003,
+
+        /// <summary>
+        /// The module
+        /// </summary>
+        [Description("Module")] Module = 1004,
+
+        /// <summary>
+        /// The menu
+        /// </summary>
+        [Description("Menu")] Menu = 1005,
+
+        /// <summary>
+        /// The dictionary setting
+        /// </summary>
+        [Description("DictionarySetting")] DictionarySetting = 1006,
+
+
+        /// <summary>
+        /// The dictionary data set
+        /// </summary>
+        [Description("DictionaryDataSet")] DictionaryDataSet = 1007,
+
+
+        /// <summary>
+        /// The dictionary
+        /// </summary>
+        [Description("Dictionary")] Dictionary = 1008,
+    }
+}
--- HKSH.Common/Constants/CapTopic.cs
+++ HKSH.Common/Constants/CapTopic.cs
@@ -9,5 +9,16 @@
         /// The audit logs
         /// </summary>
         public const string AuditLogs = "HKSH.Logs.Row.AuditLogs";
+
+        /// <summary>
+        /// The activity logs
+        /// </summary>
+        public const string ActivityLogs = "HKSH.Logs.Row.ActivityLogs";
+
+
+        /// <summary>
+        /// The action logs
+        /// </summary>
+        public const string ActionLogs = "HKSH.Logs.Row.ActionLogs";
     }
 }
