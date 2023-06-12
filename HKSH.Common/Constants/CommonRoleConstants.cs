ddf871ce52650ea260ddd03dad2abcc741c43ff8
Xiaohua <190094721@qq.com>
2023-06-12 09:41:38 +0800

    add constants

---
+++ HKSH.Common/Constants/CommonRoleConstants.cs
@@ -0,0 +1,15 @@
+﻿//  Mou,Xiaohua 2023/06/12 9:37
+
+namespace HKSH.Common.Constants
+{
+    /// <summary>
+    /// role constants
+    /// </summary>
+    public static class CommonRoleConstants
+    {
+        /// <summary>
+        /// admin role code
+        /// </summary>
+        public  const string AdminRoleCode = "r_supper_admin";
+    }
+}
