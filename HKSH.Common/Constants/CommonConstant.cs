c935f958287a60591c673ddea554e082b665a8c6
adu <adu@deloitte.com.cn>
2023-06-13 10:15:08 +0800

    更新通用库

---
+++ HKSH.Common/Constants/CommonConstant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKSH.Common.Constants
+{
+    /// <summary>
+    /// CommonConstant
+    /// </summary>
+    public class CommonConstant
+    {
+
+        /// <summary>
+        /// The ex common import data MSG
+        /// </summary>
+        public const string EX_COMMON_IMPORT_DATA_MSG = "ex_common_import_data_msg";
+
+        /// <summary>
+        /// The queue common import data MSG
+        /// </summary>
+        public const string QUEUE_COMMON_IMPORT_DATA_MSG = "queue_common_import_data_msg";
+
+        /// <summary>
+        /// The key common import data MSG
+        /// </summary>
+        public const string KEY_COMMON_IMPORT_DATA_MSG = "key_common_import_data_msg";
+    }
+}
