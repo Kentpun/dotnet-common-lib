7f45f681ae664f79bc845392d84dd0943645c5bb
Xiaohua <190094721@qq.com>
2023-06-14 15:04:36 +0800

    添加自定义业务异常

---
+++ HKSH.Common/Exception/BusinessException.cs
@@ -0,0 +1,47 @@
+﻿//  Mou,Xiaohua 2023/06/14 15:00
+
+using System;
+using System.Runtime.Serialization;
+
+namespace HKSH.Common.Exception
+{
+    /// <summary>
+    /// business exception
+    /// </summary>
+    [Serializable]
+    public class BusinessException : System.Exception
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public BusinessException()
+        {
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected BusinessException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="message"></param>
+        public BusinessException(string? message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public BusinessException(string? message, System.Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
