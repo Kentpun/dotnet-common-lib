c36ef27cc262c386bb60564ffffe059223a02078
Xiaohua <190094721@qq.com>
2023-07-03 18:31:30 +0800

    添加日志

--- HKSH.Common/XxlJob/XxlJobExecutorMiddleware.cs
+++ HKSH.Common/XxlJob/XxlJobExecutorMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿using DotXxlJob.Core;
+using HKSH.Common.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Nest;
 
 namespace HKSH.Common.XxlJob
@@ -26,15 +28,21 @@ namespace HKSH.Common.XxlJob
         private readonly XxlRestfulServiceHandler _rpcService;
 
         /// <summary>
+        /// logger
+        /// </summary>
+        private readonly ILogger<XxlJobExecutorMiddleware> _logger;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="XxlJobExecutorMiddleware"/> class.
         /// </summary>
         /// <param name="provider">The provider.</param>
         /// <param name="next">The next.</param>
-        public XxlJobExecutorMiddleware(IServiceProvider provider, RequestDelegate next)
+        public XxlJobExecutorMiddleware(IServiceProvider provider, RequestDelegate next, ILogger<XxlJobExecutorMiddleware> logger)
         {
             _provider = provider;
             _next = next;
             _rpcService = _provider.GetRequiredService<XxlRestfulServiceHandler>();
+            _logger = logger;
         }
 
         /// <summary>
@@ -43,8 +51,13 @@ namespace HKSH.Common.XxlJob
         /// <param name="context">The context.</param>
         public async Task Invoke(HttpContext context)
         {
+            
+            _logger.LogInfo("=======" + context.Request.Host.Host);
+            
             if (context.Request.Headers.Any(s => s.Key == "Authorization"))
             {
+                _logger.LogInfo("进入 ==== app url");
+                
                 await _next.Invoke(context);
             }
             else
@@ -53,8 +66,8 @@ namespace HKSH.Common.XxlJob
 
                 if ("POST".Equals(context.Request.Method, StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(contentType) && contentType.ToLower().StartsWith("application/json"))
                 {
+                    _logger.LogInfo("进入 ==== xxl job url");
                     await _rpcService.HandlerAsync(context.Request, context.Response);
-                    return;
                 }
             }
         }
