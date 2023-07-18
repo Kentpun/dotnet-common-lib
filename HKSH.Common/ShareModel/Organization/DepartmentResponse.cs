f44ce2455fac004530cdca278fbb45840b6604fc
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-07-18 10:40:30 +0800

    Format Code And Update DepartmentResponse

--- HKSH.Common/ShareModel/Organization/DepartmentResponse.cs
+++ HKSH.Common/ShareModel/Organization/DepartmentResponse.cs
@@ -84,5 +84,53 @@
         /// The department alias.
         /// </value>
         public string? DepartmentAlias { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the location full name en.
+        /// </summary>
+        /// <value>
+        /// The location full name en.
+        /// </value>
+        public string? LocationFullNameEn { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the location full name tc.
+        /// </summary>
+        /// <value>
+        /// The location full name tc.
+        /// </value>
+        public string? LocationFullNameTc { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the type of the department.
+        /// </summary>
+        /// <value>
+        /// The type of the department.
+        /// </value>
+        public string? DepartmentType { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the phone.
+        /// </summary>
+        /// <value>
+        /// The phone.
+        /// </value>
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the fax.
+        /// </summary>
+        /// <value>
+        /// The fax.
+        /// </value>
+        public string? Fax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string? Email { get; set; }
     }
 }
