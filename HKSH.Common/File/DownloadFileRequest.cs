2957913933c93e5137a453ec3fbb51582c8526da
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-07-24 15:50:19 +0800

    Add DownloadFileRequest

---
+++ HKSH.Common/File/DownloadFileRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace HKSH.Common.File
+{
+    /// <summary>
+    /// DownloadFileRequest
+    /// </summary>
+    public class DownloadFileRequest
+    {
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
+        public string Key { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        /// <value>
+        /// The name of the file.
+        /// </value>
+        public string FileName { get; set; } = null!;
+    }
+}
