80bd20fdc076bf55d91a398f00b29814df58ebae
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-08-01 15:47:13 +0800

    1

---
+++ HKSH.Common/ShareModel/NameCodeResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace HKSH.Common.ShareModel
+{
+    /// <summary>
+    /// NameCodeResponse
+    /// </summary>
+    public class NameCodeResponse
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        /// <value>
+        /// The code.
+        /// </value>
+        public string Code { get; set; } = null!;
+    }
+}
