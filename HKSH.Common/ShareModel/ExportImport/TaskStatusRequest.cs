cc771b62031ba2cb8734b4282b17f4da7e51171c
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-08-01 16:58:32 +0800

    1

---
+++ HKSH.Common/ShareModel/ExportImport/TaskStatusRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace HKSH.Common.ShareModel.ExportImport
+{
+    /// <summary>
+    /// TaskStatusRequest
+    /// </summary>
+    public class TaskStatusRequest
+    {
+        /// <summary>
+        /// Gets or sets the task code.
+        /// </summary>
+        /// <value>
+        /// The task code.
+        /// </value>
+        public string? TaskCode { get; set; }
+    }
+}
