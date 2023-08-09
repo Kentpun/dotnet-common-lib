6a9e2d147a8eb51564ea4aef40f2f7792aeb92cf
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-08-09 11:10:14 +0800

    add MdmExtension

---
+++ HKSH.Common/Extensions/MdmExtension.cs
@@ -0,0 +1,51 @@
+﻿using HKSH.Common.ShareModel.Mdm;
+using System.Text.Json;
+
+namespace HKSH.Common.Extensions
+{
+    /// <summary>
+    /// MdmExtension
+    /// </summary>
+    public static class MdmExtension
+    {
+        /// <summary>
+        /// Deserializes the data json.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="masters">The masters.</param>
+        /// <returns></returns>
+        public static List<T> DeserializeMdmDataJson<T>(this List<DictMasterResponse>? masters) where T : class
+        {
+            if (masters == null)
+            {
+                return new List<T>();
+            }
+
+            masters = masters.Where(s => s.IsActive).ToList();
+
+            if (masters == null || !masters.Any())
+            {
+                return new List<T>();
+            }
+
+            List<T> datas = new();
+            foreach (var item in masters)
+            {
+                if (string.IsNullOrEmpty(item.DataJson))
+                {
+                    continue;
+                }
+
+                var obj = JsonSerializer.Deserialize<T>(item.DataJson);
+                if (obj == null)
+                {
+                    continue;
+                }
+
+                datas.Add(obj);
+            }
+
+            return datas;
+        }
+    }
+}
