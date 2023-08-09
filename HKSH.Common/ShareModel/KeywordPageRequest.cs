615aa041da73252fd9cb39349df5d35192921707
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-08-09 18:13:49 +0800

    KeywordPageRequest

---
+++ HKSH.Common/ShareModel/KeywordPageRequest.cs
@@ -0,0 +1,19 @@
+﻿using HKSH.Common.ShareModel;
+
+namespace HKSH.Common.ShareModel
+{
+    /// <summary>
+    /// KeywordPageRequest
+    /// </summary>
+    /// <seealso cref="PaginationRequest" />
+    public class KeywordPageRequest : PaginationRequest
+    {
+        /// <summary>
+        /// Gets or sets the keyword.
+        /// </summary>
+        /// <value>
+        /// The keyword.
+        /// </value>
+        public string? Keyword { get; set; }
+    }
+}
