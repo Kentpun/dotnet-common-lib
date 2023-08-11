379767bf9279ab83260c43b60289a9f8460e6e34
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-08-11 11:44:41 +0800

    1

---
+++ HKSH.Common/ShareModel/User/UserInfoListRequest.cs
@@ -0,0 +1,48 @@
+﻿namespace HKSH.Common.ShareModel.User
+{
+    /// <summary>
+    /// UserInfo List Request
+    /// </summary>
+    public class UserInfoListRequest
+    {
+        /// <summary>
+        /// Gets or sets the keyword.
+        /// </summary>
+        /// <value>
+        /// The keyword.
+        /// </value>
+        public string? Keyword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the group.
+        /// </summary>
+        /// <value>
+        /// The group.
+        /// </value>
+        public string? Group { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location code.
+        /// </summary>
+        /// <value>
+        /// The location code.
+        /// </value>
+        public string? LocationCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department code.
+        /// </summary>
+        /// <value>
+        /// The department code.
+        /// </value>
+        public string? DepartmentCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the role.
+        /// </summary>
+        /// <value>
+        /// The type of the role.
+        /// </value>
+        public string? RoleType { get; set; }
+    }
+}
---
+++ HKSH.Common/ShareModel/User/UserInfoPageRequest.cs
@@ -0,0 +1,41 @@
+﻿namespace HKSH.Common.ShareModel.User
+{
+    /// <summary>
+    /// UserInfo Page Request
+    /// </summary>
+    /// <seealso cref="KeywordPageRequest" />
+    public class UserInfoPageRequest : KeywordPageRequest
+    {
+        /// <summary>
+        /// Gets or sets the group.
+        /// </summary>
+        /// <value>
+        /// The group.
+        /// </value>
+        public string? Group { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location code.
+        /// </summary>
+        /// <value>
+        /// The location code.
+        /// </value>
+        public string? LocationCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department code.
+        /// </summary>
+        /// <value>
+        /// The department code.
+        /// </value>
+        public string? DepartmentCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the role.
+        /// </summary>
+        /// <value>
+        /// The type of the role.
+        /// </value>
+        public string? RoleType { get; set; }
+    }
+}
