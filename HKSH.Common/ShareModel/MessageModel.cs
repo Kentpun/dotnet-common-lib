e369a667383571bfeb0874da5bf325e8cbdf8667
Tan,Changyang <chantan@deloitte.com.cn>
2023-08-11 10:58:10 +0800

    remove system message

--- HKSH.Common/ShareModel/MessageModel.cs
+++ HKSH.Common/ShareModel/MessageModel.cs
@@ -29,5 +29,167 @@ namespace HKSH.Common.ShareModel
         /// </summary>
         public string Code { get; set; }
 
+        #region default message
+
+        #region Common
+
+        /// <summary>
+        /// Gets the success.
+        /// </summary>
+        /// <value>
+        /// The success.
+        /// </value>
+        public static MessageModel Success => new MessageModel("000000");
+
+        /// <summary>
+        /// Gets the failure.
+        /// </summary>
+        /// <value>
+        /// The failure.
+        /// </value>
+        public static MessageModel Failure => new MessageModel("999999");
+
+        #endregion
+
+        #region Permission
+
+        /// <summary>
+        /// Gets the system is busy.
+        /// </summary>
+        /// <value>
+        /// The system is busy.
+        /// </value>
+        public static MessageModel TheSystemIsBusy => new MessageModel("000061");
+
+        /// <summary>
+        /// Gets the actions are too frequent.
+        /// </summary>
+        /// <value>
+        /// The actions are too frequent.
+        /// </value>
+        public static MessageModel ActionsAreTooFrequent => new MessageModel("000062");
+
+        #endregion 
+
+        #region Param
+
+        /// <summary>
+        /// Gets the required parameter is empty.
+        /// </summary>
+        /// <value>
+        /// The required parameter is empty.
+        /// </value>
+        public static MessageModel TheRequiredParameterIsEmpty => new MessageModel("000040");
+
+        /// <summary>
+        /// Gets the required parameter is invalid.
+        /// </summary>
+        /// <value>
+        /// The required parameter is invalid.
+        /// </value>
+        public static MessageModel TheRequiredParameterIsInvalid => new MessageModel("000041");
+
+        /// <summary>
+        /// Gets the data is invalid.
+        /// </summary>
+        /// <value>
+        /// The data is invalid.
+        /// </value>
+        public static MessageModel TheDataIsInvalid => new MessageModel("000042");
+
+        /// <summary>
+        /// Gets the signature verification failed.
+        /// </summary>
+        /// <value>
+        /// The signature verification failed.
+        /// </value>
+        public static MessageModel SignatureVerificationFailed => new MessageModel("000043");
+
+        /// <summary>
+        /// Gets the token mismatch.
+        /// </summary>
+        /// <value>
+        /// The token mismatch.
+        /// </value>
+        public static MessageModel TokenMismatch => new MessageModel("000044");
+
+        /// <summary>
+        /// Gets the token has lapsed.
+        /// </summary>
+        /// <value>
+        /// The token has lapsed.
+        /// </value>
+        public static MessageModel TheTokenHasLapsed => new MessageModel("000045");
+
+        /// <summary>
+        /// Gets the request parameter is incorrect.
+        /// </summary>
+        /// <value>
+        /// The request parameter is incorrect.
+        /// </value>
+        public static MessageModel TheRequestParameterIsIncorrect => new MessageModel("000046");
+
+
+        #endregion 
+
+        #region System
+        /// <summary>
+        /// Gets the user does not have permission to do this.
+        /// </summary>
+        /// <value>
+        /// The user does not have permission to do this.
+        /// </value>
+        public static MessageModel TheUserDoesNotHavePermissionToDoThis => new MessageModel("000090");
+
+        /// <summary>
+        /// Gets the downstream system response timed out.
+        /// </summary>
+        /// <value>
+        /// The downstream system response timed out.
+        /// </value>
+        public static MessageModel TheDownstreamSystemResponseTimedOut => new MessageModel("000091");
+
+        /// <summary>
+        /// Gets the downstream system cannot find or cannot reach.
+        /// </summary>
+        /// <value>
+        /// The downstream system cannot find or cannot reach.
+        /// </value>
+        public static MessageModel TheDownstreamSystemCannotFindOrCannotReach => new MessageModel("000092");
+
+        /// <summary>
+        /// Gets the database connection error.
+        /// </summary>
+        /// <value>
+        /// The database connection error.
+        /// </value>
+        public static MessageModel DatabaseConnectionError => new MessageModel("000093");
+
+        /// <summary>
+        /// Gets the database operation exception.
+        /// </summary>
+        /// <value>
+        /// The database operation exception.
+        /// </value>
+        public static MessageModel DatabaseOperationException => new MessageModel("000094");
+
+        /// <summary>
+        /// Gets the calling downstream system is unresponsive.
+        /// </summary>
+        /// <value>
+        /// The calling downstream system is unresponsive.
+        /// </value>
+        public static MessageModel TheCallingDownstreamSystemIsUnresponsive => new MessageModel("000095");
+
+        /// <summary>
+        /// Gets the unknown exception.
+        /// </summary>
+        /// <value>
+        /// The unknown exception.
+        /// </value>
+        public static MessageModel UnknownException => new MessageModel("000099");
+        #endregion
+
+        #endregion
     }
 }
--- HKSH.Common/ShareModel/MessageResult.cs
+++ HKSH.Common/ShareModel/MessageResult.cs
@@ -57,7 +57,7 @@
                 Success = true,
                 Message = "Operation successful",
                 Module = "System",
-                Code = SystemMessage.Success.Code
+                Code = MessageModel.Success.Code
             };
         }
 
@@ -76,7 +76,7 @@
                 Success = true,
                 Message = string.IsNullOrEmpty(message) ? "Operation successful" : message,
                 Module = string.IsNullOrEmpty(module) ? "System" : module,
-                Code = messageModel?.Code ?? SystemMessage.Success.Code
+                Code = messageModel?.Code ?? MessageModel.Success.Code
             };
         }
 
@@ -96,7 +96,7 @@
                 Success = true,
                 Message = string.IsNullOrEmpty(message) ? "Operation failed" : message,
                 Module = string.IsNullOrEmpty(module) ? "System" : module,
-                Code = messageModel?.Code ?? SystemMessage.Success.Code
+                Code = messageModel?.Code ?? MessageModel.Success.Code
             };
         }
 
@@ -109,7 +109,7 @@
             return new MessageResult
             {
                 Success = false,
-                Code = SystemMessage.Failure.Code,
+                Code = MessageModel.Failure.Code,
                 Message = "Operation failed",
                 Module = string.IsNullOrEmpty(module) ? "System" : module
             };
@@ -149,7 +149,7 @@
                 Success = false,
                 Message = string.IsNullOrEmpty(message) ? "Operation failed" : message,
                 Module = string.IsNullOrEmpty(module) ? "System" : module,
-                Code = messageModel?.Code ?? SystemMessage.Success.Code
+                Code = messageModel?.Code ?? MessageModel.Success.Code
             };
         }
 
@@ -185,7 +185,7 @@
                 Data = data,
                 Message = string.IsNullOrEmpty(message) ? "Operation successful" : message,
                 Module = string.IsNullOrEmpty(module) ? "System" : module,
-                Code = messageModel?.Code ?? SystemMessage.Success.Code
+                Code = messageModel?.Code ?? MessageModel.Success.Code
             };
         }
 
@@ -205,7 +205,7 @@
                 Data = data ?? default,
                 Message = string.IsNullOrEmpty(message) ? "Operation failed" : message,
                 Module = string.IsNullOrEmpty(module) ? "System" : module,
-                Code = messageModel?.Code ?? SystemMessage.Failure.Code,
+                Code = messageModel?.Code ?? MessageModel.Failure.Code,
             };
         }
     }
