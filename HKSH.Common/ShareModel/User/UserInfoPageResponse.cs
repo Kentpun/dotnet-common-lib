57976b4cf1c3e1dd7e69459a75bcf912fffcf99d
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-08-14 18:45:41 +0800

    update UserInfoPageResponse

--- HKSH.Common/ShareModel/User/UserInfoPageResponse.cs
+++ HKSH.Common/ShareModel/User/UserInfoPageResponse.cs
@@ -119,6 +119,118 @@ namespace HKSH.Common.ShareModel.User
         public string LocationCode { get; set; } = string.Empty;
 
         /// <summary>
+        /// Gets or sets the location full name en.
+        /// </summary>
+        /// <value>
+        /// The location full name en.
+        /// </value>
+        public string? LocationFullNameEn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location full name tc.
+        /// </summary>
+        /// <value>
+        /// The location full name tc.
+        /// </value>
+        public string? LocationFullNameTc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location short name en.
+        /// </summary>
+        /// <value>
+        /// The location short name en.
+        /// </value>
+        public string? LocationShortNameEn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location alias.
+        /// </summary>
+        /// <value>
+        /// The location alias.
+        /// </value>
+        public string? LocationAlias { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department code.
+        /// </summary>
+        /// <value>
+        /// The department code.
+        /// </value>
+        public string DepartmentCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the department full name en.
+        /// </summary>
+        /// <value>
+        /// The department full name en.
+        /// </value>
+        public string? DepartmentFullNameEn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department full name tc.
+        /// </summary>
+        /// <value>
+        /// The department full name tc.
+        /// </value>
+        public string? DepartmentFullNameTc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department short name en.
+        /// </summary>
+        /// <value>
+        /// The department short name en.
+        /// </value>
+        public string? DepartmentShortNameEn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department alias.
+        /// </summary>
+        /// <value>
+        /// The department alias.
+        /// </value>
+        public string? DepartmentAlias { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization code.
+        /// </summary>
+        /// <value>
+        /// The organization code.
+        /// </value>
+        public string OrganizationCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the organization name en.
+        /// </summary>
+        /// <value>
+        /// The organization name en.
+        /// </value>
+        public string? OrganizationNameEn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization name tc.
+        /// </summary>
+        /// <value>
+        /// The organization name tc.
+        /// </value>
+        public string? OrganizationNameTc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization short name en.
+        /// </summary>
+        /// <value>
+        /// The organization short name en.
+        /// </value>
+        public string? OrganizationShortNameEn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization short name tc.
+        /// </summary>
+        /// <value>
+        /// The organization short name tc.
+        /// </value>
+        public string? OrganizationShortNameTc { get; set; }
+
+        /// <summary>
         /// Gets or sets the status.
         /// </summary>
         /// <value>
