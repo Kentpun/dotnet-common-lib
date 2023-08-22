5a71467a4fc0c8fd81af8bb7ef3cfb511964c361
adu <adu@deloitte.com.cn>
2023-08-22 14:15:39 +0800

    add res

---
+++ HKSH.Common/ShareModel/ExportImport/TaskResponse.cs
@@ -0,0 +1,81 @@
+﻿namespace HKSH.Common.ShareModel.ExportImport
+{
+    /// <summary>
+    /// TaskResponse
+    /// </summary>
+    public class TaskResponse
+    {
+        /// <summary>
+        /// Gets or sets the task code.
+        /// </summary>
+        /// <value>
+        /// The task code.
+        /// </value>
+        public string? TaskCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the acm.
+        /// </summary>
+        /// <value>
+        /// The type of the acm.
+        /// </value>
+        public Common.Enums.TaskType TaskType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action.
+        /// </summary>
+        /// <value>
+        /// The action.
+        /// </value>
+        public Common.Enums.ExportImportType Action { get; set; }
+
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        /// <value>
+        /// The name of the file.
+        /// </value>
+        public string FileName { get; set; } = String.Empty;
+
+
+        /// <summary>
+        /// Gets or sets the name of the bucket.
+        /// </summary>
+        /// <value>
+        /// The name of the bucket.
+        /// </value>
+        public string BucketName { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Gets or sets the name of the object.
+        /// </summary>
+        /// <value>
+        /// The name of the object.
+        /// </value>
+        public string ObjectName { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Gets or sets the status.
+        /// </summary>
+        /// <value>
+        /// The status.
+        /// </value>
+        public Common.Enums.TaskStatus Status { get; set; }
+
+        /// <summary>
+        /// 状态变更原因
+        /// </summary>
+        /// <value>
+        /// The remark.
+        /// </value>
+        public string Remark { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Gets or sets the created at.
+        /// </summary>
+        /// <value>
+        /// The created at.
+        /// </value>
+        public DateTime CreatedAt { get; set; }
+    }
+}
