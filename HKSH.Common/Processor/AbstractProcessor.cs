023837248f46a142b36284df6828471b779ea313
Xiaohua <190094721@qq.com>
2023-09-07 18:29:15 +0800

    add processor

---
+++ HKSH.Common/Processor/AbstractProcessor.cs
@@ -0,0 +1,58 @@
+﻿namespace HKSH.Common.Processor
+{
+    /// <summary>
+    /// abstract processor
+    /// </summary>
+    /// <typeparam name="R"></typeparam>
+    /// <typeparam name="I"></typeparam>
+    public abstract class AbstractProcessor<R, I> : IProcessor<R, I> where R : class where I : class
+    {
+        /// <summary>
+        /// Processes the specified input.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="processCtx">The process CTX.</param>
+        /// <returns></returns>
+        public R Process(I input, ProcessCtx processCtx)
+        {
+            R result = ProcessInternal(input, processCtx);
+
+            if (HasNextProcessor(input, result, processCtx))
+            {
+                ProcessNext(input, result, processCtx);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Determines whether [has next processor] [the specified input].
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="result">The result.</param>
+        /// <param name="processCtx">The process CTX.</param>
+        /// <returns>
+        ///   <c>true</c> if [has next processor] [the specified input]; otherwise, <c>false</c>.
+        /// </returns>
+        protected virtual bool HasNextProcessor(I input, R result, ProcessCtx processCtx)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Processes the internal.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="processCtx">The process CTX.</param>
+        /// <returns></returns>
+        protected abstract R ProcessInternal(I input, ProcessCtx processCtx);
+
+        /// <summary>
+        /// Processes the next.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="result">The result.</param>
+        /// <param name="processCtx">The process CTX.</param>
+        protected abstract void ProcessNext(I input, R result, ProcessCtx processCtx);
+    }
+}
---
+++ HKSH.Common/Processor/IProcessor.cs
@@ -0,0 +1,18 @@
+﻿namespace HKSH.Common.Processor
+{
+    /// <summary>
+    /// processor interface
+    /// </summary>
+    /// <typeparam name="R"></typeparam>
+    /// <typeparam name="I"></typeparam>
+    public interface IProcessor<R, I> where R : class where I : class
+    {
+        /// <summary>
+        /// Processes the specified input.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="processCtx">The process CTX.</param>
+        /// <returns></returns>
+        R Process(I input, ProcessCtx processCtx);
+    }
+}
