558fc06b21f0cce0b27c69a15f32b498199f9c08
Leo Feihong Liu <feiliu@deloitte.com.cn>
2023-11-08 00:02:57 +0800

    Add CentreLocationRequest

---
+++ HKSH.Common/ShareModel/Organization/CentreLocationRequest.cs
@@ -0,0 +1,27 @@
+﻿using HKSH.Common.ShareModel.Request;
+
+namespace HKSH.Common.ShareModel.Organization
+{
+    /// <summary>
+    /// CentreLocationRequest
+    /// </summary>
+    /// <seealso cref="KeywordPageRequest" />
+    public class CentreLocationRequest : KeywordPageRequest
+    {
+        /// <summary>
+        /// Gets or sets the site code.
+        /// </summary>
+        /// <value>
+        /// The site code.
+        /// </value>
+        public string? SiteCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location codes.
+        /// </summary>
+        /// <value>
+        /// The location codes.
+        /// </value>
+        public List<string>? LocationCodes { get; set; }
+    }
+}
